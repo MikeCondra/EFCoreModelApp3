d46229c048ab31e0c2bfd74fd5f3d57fe7d3253d
Michael <michael.condra@gmail.com>
2025-02-07 12:34:40 -0800

    Added employees

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.Serialization;
+using EFCoreModelApp3.Models;
 
 namespace EFCoreModelApp3
 {
@@ -7,7 +8,35 @@ namespace EFCoreModelApp3
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var context = new CompanyContext();
+
+            var department = new Department { Name = "Engineering" };
+
+            context.Departments.Add(
+                new Department
+                {
+                    Name = "Engineering",
+                    Employees = new List<Employee>
+                {
+                    new Employee
+                    {
+                        FirstName = "Marissa",
+                        LastName = "Johanssen",
+                        HireDate = DateTime.Now
+                    },
+                    new Employee
+                    {
+                        FirstName = "Ryan",
+                        LastName = "Ford",
+                        HireDate = DateTime.Now
+                    }
+                }
+                });
+
+            context.SaveChanges();
+
+            var employees = context.Employees.Where(e => e.Department != null && e.Department.Name == "Engineering").ToList();
+            Console.WriteLine($"Employees in Engineering:  {employees.Count}");
         }
     }
 }
